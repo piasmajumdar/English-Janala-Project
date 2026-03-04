022488e08445f862a87d72a3993f15184672eee2
piasmajumdar <piasmajumdar@gmail.com>
2026-03-04 19:58:40 +0530

    search button added

--- script/index.js
+++ script/index.js
@@ -154,4 +154,21 @@ const displayLesson = (lessons) => {
     }
 }
 
-loadLessons()
+loadLessons();
+
+document.getElementById("btn-search").addEventListener('click', ()=>{
+    removeActive();
+
+    const input = document.getElementById("input-search");
+    const searchValue = input.value.trim().toLowerCase();
+    console.log(searchValue);
+
+    fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((res)=> res.json())
+    .then((data)=> {
+        const allWords = data.data;
+        const filterWords = allWords.filter(word=> word.word.toLowerCase().includes(searchValue));
+        displayLevelWord(filterWords);
+
+    })
+})
