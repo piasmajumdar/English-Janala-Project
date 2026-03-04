d0aa5cd347427876eeaeea6baf49937520cdc8cc
piasmajumdar <piasmajumdar@gmail.com>
2026-03-04 06:17:07 +0530

    levels name collected by API

---
+++ script/index.js
@@ -0,0 +1,29 @@
+const loadLessions = () => {
+    const url = "https://openapi.programming-hero.com/api/levels/all";
+    fetch(url) // promise of response
+    .then((res) => res.json()) //promise of json
+    .then((json)=> displayLesson(json.data))
+};
+
+const displayLesson = (lessons) => {
+    // 1. get the container & empty
+    const levelContainer = document.getElementById('level-container');
+    levelContainer.innerHTML = "";
+
+    // 2. get into every lessons
+    for(let lesson of lessons){
+        
+        //     3. create Element
+        const btnDiv = document.createElement('div');
+        btnDiv.innerHTML = `
+            <button href="" class="btn btn-outline btn-primary">
+                <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
+            </button>
+        `;
+        //     4. Append into container
+        levelContainer.append(btnDiv);
+        
+    }
+}
+
+loadLessions()
